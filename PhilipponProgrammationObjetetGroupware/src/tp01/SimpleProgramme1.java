7167bcbbc95e86babeb0b17b9121eeae6f6d89b6
rophi <rophi@LAPTOP-ORBJLDFD>
2022-10-20 11:08:20 +0200

    atelier 5

--- PhilipponProgrammationObjetetGroupware/src/tp01/SimpleProgramme1.java
+++ PhilipponProgrammationObjetetGroupware/src/tp01/SimpleProgramme1.java
@@ -1,6 +1,13 @@
 package tp01;
-
+ 
+/** 
+ * @author rophi
+ * 
+ */
 public class SimpleProgramme1 {
+	/**
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- PhilipponProgrammationObjetetGroupware/src/tp05/testCarafe.java
+++ PhilipponProgrammationObjetetGroupware/src/tp05/testCarafe.java
@@ -1,15 +1,20 @@
 package tp05;
 
+import tp05.Carafe;
+
 public class testCarafe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Carafe c1 = new Carafe (0,7);
-		Carafe c2 = new Carafe (0,5);
+		Carafe c1 = new Carafe (0, 7);
+		Carafe c2 = new Carafe (0, 5);
 		
 		c1.remplir();
 		c2.vider();
 		
+		System.out.println(c1.contenu());
+		System.out.println(c2.capacite());
+	
 	}
 
 }
+
